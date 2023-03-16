6928a2b07b5c56646ebc51ee92d02003df64754e
dohyunkim <kdhminime@gmail.com>
2023-03-16 12:59:41 -0600

    search bar in progress

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,24 +1,77 @@
-import { TextField } from "@mui/material";
+import {
+  TextField,
+  Menu,
+  MenuItem,
+  Paper,
+  MenuList,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
 import React from "react";
 import SearchIcon from "@mui/icons-material/Search";
-import { InputAdornment, IconButton } from "@mui/material";
+import { InputAdornment } from "@mui/material";
 import "../css/TopLinks.css";
+import { useState } from "react";
+import HistoryIcon from "@mui/icons-material/History";
+import { alpha, styled } from "@mui/material/styles";
+
+const CssTextField = styled(TextField)({
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": {
+      borderColor: "#e2e2e2",
+    },
+    "&.Mui-focused fieldset": {
+      borderColor: "#e2e2e2",
+      borderWidth: "0.5pt",
+    },
+  },
+});
 
 let SearchBox = () => {
+  const [open, setOpen] = useState(false);
+  const [borderStyle, setBorderStyle] = useState("textFieldClose");
+
+  const handleClick = () => {
+    if (open) {
+      setOpen(false);
+      setBorderStyle("textFieldClose");
+    } else {
+      setOpen(true);
+      setBorderStyle("textFieldOpen");
+    }
+  };
+
+  let SearchHistory = () => {
+    return (
+      <Paper sx={{ width: "44%", borderRadius: "0pt" }}>
+        <MenuList>
+          <MenuItem>
+            <ListItemIcon>
+              <HistoryIcon />
+            </ListItemIcon>
+            <ListItemText>Netflix</ListItemText>
+          </MenuItem>
+        </MenuList>
+      </Paper>
+    );
+  };
+
   return (
     <div className="searchBoxContainer">
-      <TextField
-        className="textField"
+      <CssTextField
+        style={{ width: "44%" }}
+        onFocus={handleClick}
+        onBlur={handleClick}
+        className={borderStyle}
         InputProps={{
           startAdornment: (
             <InputAdornment position="start">
-              <IconButton>
-                <SearchIcon />
-              </IconButton>
+              <SearchIcon />
             </InputAdornment>
           ),
         }}
       />
+      {open ? <SearchHistory /> : null}
     </div>
   );
 };
